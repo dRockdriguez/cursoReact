1c74cdb35ab242861b85a6d50f3f335c06cde60a
David Rodriguez <david.rodriguez14.contractor@bbva.com>
2022-05-20 20:33:06 +0200

    fin seccion 18

--- 7-JournalApp/journal-app/src/components/notes/NotesScreen.js
+++ 7-JournalApp/journal-app/src/components/notes/NotesScreen.js
@@ -1,10 +1,32 @@
-import React from 'react'
-import { NotesAppBar } from './NotesAppBar'
+import React from "react";
+import { NotesAppBar } from "./NotesAppBar";
 
 export const NotesScreen = () => {
   return (
     <div className="notes__main-content">
-      <NotesAppBar/>
+      <NotesAppBar />
+
+      <form className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__content-input"
+          autoComplete="off"
+        />
+
+        <textarea
+          placeholder="What happend today?"
+          className="notes__content-textarea"
+        ></textarea>
+
+        <div className="notes__image">
+          <img
+            src="https://fotografiamejorparavendermas.com/wp-content/uploads/2017/06/La-importancia-de-la-imagen.jpg"
+            alt="imagen"
+          />
+          "
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
